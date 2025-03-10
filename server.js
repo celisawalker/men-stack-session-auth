a91d77ff956d9bac41cb4169db0a2cb491067218
celisawalker <celisa.walker@gmail.com>
2025-03-10 16:28:40 -0700

    mounted session  middleware

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
+const authController = require("./controllers/auth");
+const session = require("express-session");
 
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -23,11 +30,19 @@ app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
 
+//FUN FACT! router code is actually a type of middleware!
+app.use("/auth", authController);
+//any HTTP requests that come to /auth will automatically 
+// be forwarded to the router code inside of the authController
+
 //mount routes
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+    res.render("index.ejs", {
+        user: req.session.user
+    });
+});
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
